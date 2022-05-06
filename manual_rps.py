ec3504fba3ceedc7052f261435dbb727b6814070
kofimensah <kofi.mensah@hotmail.com>
2022-05-06 22:50:26 +0100

    function is created to determine winner based on choice pairings

--- manual_rps.py
+++ manual_rps.py
@@ -9,5 +9,22 @@ def get_computer_choice():
     return ans
 
 def get_user_choice():
-    guess = input("Please guess the object: Rock, Paper or Scissors")
+    guess = input("Please guess the object: Rock, Paper or Scissors").capitalize()
     return guess 
+
+def get_winner(computer_choice, user_choice):
+    print(f"Computer = {computer_choice}, User = {user_choice}")
+    if computer_choice == "Rock" and user_choice == "Paper":
+        return "User Wins"
+    elif computer_choice == "Rock" and user_choice == "Scissors":
+        return "Computer Wins"
+    elif computer_choice == "Paper" and user_choice == "Rock":
+        return "Computer Wins"
+    elif computer_choice == "Paper" and user_choice == "Scissors":
+        return "User Wins"
+    elif computer_choice == "Scissors" and user_choice == "Rock":
+        return "User Wins"
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+        return "Computer Wins"
+    else:
+        return "Try Again"
