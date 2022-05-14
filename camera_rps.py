c060b9a51f8c57bfa3f24ccf5d56d8dc06fff57e
kofimensah <kofi.mensah@hotmail.com>
2022-05-15 00:19:08 +0100

    copied the input file to run the camera rps

---
+++ camera_rps.py
@@ -0,0 +1,31 @@
+from configparser import Interpolation
+from ctypes import resize
+from xml.etree.ElementPath import prepare_predicate
+from xmlrpc.server import list_public_methods
+
+import cv2
+from keras.models import load_model
+import numpy as np
+
+model = load_model('karas_model.h5')
+cap = cv2.VideoCapture(0)
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+while True:
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 #Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    cv2.imshow('frame', frame)
+    #press q to close the window
+    print(prediction)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
