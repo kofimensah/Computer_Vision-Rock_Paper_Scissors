e4565feb1dd1414e637d94d3af2d917bfec732f1
kofimensah <kofi.mensah@hotmail.com>
2022-05-15 23:02:44 +0100

    Function built to read from camera and return prediction

--- camera_rps.py
+++ camera_rps.py
@@ -7,25 +7,31 @@ import cv2
 from keras.models import load_model
 import numpy as np
 
-model = load_model('karas_model.h5')
-cap = cv2.VideoCapture(0)
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+def get_prediction():
 
-while True:
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 #Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    cv2.imshow('frame', frame)
-    #press q to close the window
-    print(prediction)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    model = load_model('keras_model.h5')
+    cap = cv2.VideoCapture(0)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    labels = ['Rock','Paper','Scissors','Nothing']
+    output = []
+
+    while True:
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data)
+        cv2.imshow('frame', frame)
+        # Press q to close the window
+        output = prediction
         break
+    cap.release()
+    # Destroy the camera window
+    cv2.destroyAllWindows()
+    
+    cv_pred_arr = output[0].tolist()
+    index = cv_pred_arr.index(max(cv_pred_arr))
 
-#After the loop release the cap object
-cap.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
-
+    return labels[index]
