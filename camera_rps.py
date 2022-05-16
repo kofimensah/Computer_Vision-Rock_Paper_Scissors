0e1affafcbc0a07a80a4d43304bc27fe6aa77df5
kofimensah <kofi.mensah@hotmail.com>
2022-05-16 19:25:16 +0100

    Introducing a function to take output from the tensorflow program and return a RPS prediction

--- camera_rps.py
+++ camera_rps.py
@@ -7,35 +7,35 @@ import cv2
 from keras.models import load_model
 import numpy as np
 
-
-#This cell script should include several functions to achieve the goal. The get_prediction is the name of the final thing
-
-
-def get_prediction():
-
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
+def get_prediction(array):
+    #RPS options in a list - for easy indexing
     labels = ['Rock','Paper','Scissors','Nothing']
-    output = []
-
-    while True:
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        cv2.imshow('frame', frame)
-        # Press q to close the window
-        output = prediction
-        break
-    cap.release()
-    # Destroy the camera window
-    cv2.destroyAllWindows()
-    
-    cv_pred_arr = output[0].tolist()
-    index = cv_pred_arr.index(max(cv_pred_arr))
-
+    #conversion from array to list
+    out = array[0].tolist()
+    prob = max(out)
+    index = out.index(prob)
+    #results - fetching the item at a particular index of the RPS list
     return labels[index]
+
+#Outputting through a function that will pick output of the camera and predict r,p or s.
+model = load_model('keras_model.h5')
+cap = cv2.VideoCapture(0)
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+while True:
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 #Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    cv2.imshow('frame', frame)
+    #Press q to close the window
+    print(get_prediction(prediction))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#After the loop release the cap object
+cap.release()
+#Destrop all the windows
+cv2.destroyAllWindows()
